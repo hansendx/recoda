3a1b2ea7811141ac36042cdb12c2adf68a69a157
hansendx <hansendx@hu-berlin.de>
2018-08-18 14:25:34 +0200

    [python.metrics] Implemented 3ce24ead05c4eb7e361c6d7d3b0e895e6ef37be9
     - Initiated a python metrics module
     - Put functionality in a private module named after the quality subfactor

---
+++ recoda/analyse/python/_installability.py
@@ -0,0 +1,47 @@
+""" All metrics measuring th installability subfactor. """
+
+import os
+
+import pyroma
+import numpy
+
+from recoda.analyse.helpers import (
+    search_filename
+)
+
+def packageability(project_path: str) -> int:
+    """ Gives a score on the packageability of a python software project.
+
+    :param project: Represents a software Project somewhere in local storage.
+    """
+    _setup_files = _get_setup_location(project_path)
+
+    if len(_setup_files) == 1:
+        return pyroma.run('directory', project_path)
+    if not _setup_files:
+        # No setup.py gets a zero score
+        return 0
+
+    _score_list = []
+    for _setup_file in _setup_files:
+        _score_list.append(
+            pyroma.run('directory', project_path)
+        )
+    return numpy.mean(_score_list)
+    
+
+def _get_setup_location(path: str) -> str:
+    """ Returns a list of paths to setup.py files in a directory.
+
+    :returns: The result of a call to search_filename
+            of the recoda.analyse.helpers module
+            with path as base_folder and "setup.py"
+            as file to search.
+    """
+    _setup_file_string = 'setup.py'
+    _setup_file_list = search_filename(
+        base_folder=path,
+        file_name=_setup_file_string
+    )
+
+    return [os.path.dirname(_path) for _path in _setup_file_list]
--- recoda/analyse/python/metrics.py
+++ recoda/analyse/python/metrics.py
@@ -2,24 +2,8 @@
 """
 
 import os
-from typing import Union
-import git
+from recoda.analyse.python import (
+    _installability
+)
 
-def packageable(project: Union[str, git.repo.base.Repo]) -> int:
-    """ Gives a score on the packageability of a python software project.
-
-    :param project: Represents a software Project somewhere in local storage.
-    :raises NotADirectoryError: When project directory does not exist.
-    """
-
-    #_path = _get_project_directory(project)
-    #_setup_dir = _get_setup_location(_path)
-    return 0
-
-def _get_setup_location(path: str) -> str:
-    """ Returns a list of paths to setup.py files in a directory.
-    """
-    #TODO
-    return ''
-
-
+packageability = _installability.packageability
