b93d8bc18a36966c9ed8d94cff4c4c45de8c030e
hansendx <hansendx@hu-berlin.de>
2018-08-19 13:28:30 +0200

    ?DOCS? Added some documentation.

--- recoda/analyse/python/metrics.py
+++ recoda/analyse/python/metrics.py
@@ -6,5 +6,9 @@ from recoda.analyse.python import (
 )
 
 # pylint: disable-msg=c0103
+# For now this seems to be the most streamline method of decentralization
+# of this module. We want to call all functions via the metrics but we do
+# not want it to be too long and unreadable. Wrapping the private module
+# functions into a barebones would just lead to a lot more unnecessary code.
 
 packageability = _installability.packageability
