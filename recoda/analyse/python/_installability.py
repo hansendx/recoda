5f05c7f9165dcba4a61534712ddfc18890c69c13
hansendx <hansendx@hu-berlin.de>
2018-10-26 12:27:26 +0200

    [python.installability] Refactor requirements_declared and related

--- recoda/analyse/python/_installability.py
+++ recoda/analyse/python/_installability.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 import astroid
-import pipreqs.pipreqs as pipreqs
+from pipreqs import pipreqs
 import pyroma
 import numpy
 
@@ -38,22 +38,27 @@ def packageability(project_path: str) -> int:
     return numpy.mean(_score_list)
 
 def requirements_declared(project_path: str) -> float:
-    """ Checks, if requirements are declared for imports. """
-    _declared_requirements = _get_requirements_txt_requirements(path=project_path)
-    _setup_requirements = _get_setup_requirements(path=project_path)
-    _implied_requirements = pipreqs.get_all_imports(path=project_path)
+    """ Calculates percentage of not declared dependencies. """
+    _declared_requirements = _get_requirements_from_file(path=project_path)
+    _setup_requirements = _get_requirements_from_setup(path=project_path)
+    _implied_dependencies = pipreqs.get_all_imports(path=project_path)
+    # We cannot calculate a percentage for declared dependencies,
+    # if there are no dependencies through imports.
+    if not _implied_dependencies:
+        return None
 
-    _correctly_declared_requirements_count = len(_implied_requirements)
-    for requirement in _implied_requirements:
+    _correctly_declared_requirements_count = len(_implied_dependencies)
+    for requirement in _implied_dependencies:
         if requirement not in _declared_requirements and requirement not in _setup_requirements:
             _correctly_declared_requirements_count = _correctly_declared_requirements_count - 1
-    
-    return float(_correctly_declared_requirements_count) / len(_implied_requirements)
 
-def _get_requirements_txt_requirements(path: str) -> set:
+    return float(_correctly_declared_requirements_count) / len(_implied_dependencies)
+
+def _get_requirements_from_file(path: str) -> set:
     """ Returns a list of requirements parsed from all requirements files insode a path.
 
-    :returns: A set of all requirements found.
+    :param path: Full path to a python software project.
+    :returns:    A set of all requirements found in requirements.txt files.
     """
     _file_string = 'requirements.txt'
     _file_list = search_filename(
@@ -65,31 +70,65 @@ def _get_requirements_txt_requirements(path: str) -> set:
     for file_name in _file_list:
         for requirement in pipreqs.parse_requirements(file_=file_name):
             _requirements_content.add(requirement['name'])
-    
+
     return _requirements_content
 
-def _get_setup_requirements(path: str) -> str:
+def _get_requirements_from_setup(path: str) -> str:
+    """ Extract requirements declared in the setup.py files of a project.
+
+    If several setup.py files are contained in a project,
+    their declared requirements are concatenated.
+
+    :param path: Full path to the location of a python software project.
+    :returns:    All declared requirements of a project contained in a string.
+    """
     _setup_file_locations = _get_setup_locations(path)
     if not _setup_file_locations:
         return ''
     _setup_files = [path+'/setup.py' for path in _setup_file_locations]
     _requirements = ""
     for _setup_file in _setup_files:
-        _requirements = _requirements + _astroid_parse_for_setup(_setup_file)
+        _requirements = _requirements + _astroid_parse_setup(_setup_file)
 
     return _requirements
 
-def _astroid_parse_for_setup(path: str) -> set:
+def _astroid_parse_setup(path: str) -> str:
+    """ Extract requirements out of the setup call in a setup.py
+    
+    If for som reason several setup calls are present,
+    their requirements are concatenated
+
+    :param path: Full path to the location of a setup.py file.
+    :returns:    All declared requirements of a setup.py file contained in a string.
+    """
     _setup_file = open(path)
     _setup_content = _setup_file.read()
     _setup_file.close()
     _setup_parse_tree = astroid.parse(_setup_content)
+    _requirements_regex = r'.*install_requires=\[(.*?)\].*'
     _requirement_string = ''
-    for child in _setup_parse_tree.get_children(): 
-        if child.as_string()[:5] == 'setup': 
-            return re.sub(r'.*install_requires=\[(.*?)\].*', r'\1',  child.as_string())
+    for _setup in _astroid_setup_search(_setup_parse_tree): 
+        _requirement_string = _requirement_string + re.sub(
+            _requirements_regex, r'\1',
+            _setup
+        )
+    return _requirement_string
 
+def _astroid_setup_search(node: astroid.nodes) -> list:
+    """ Search for the setup function calls in a astroid tree recursively.
 
+    :param node: A astroid node object containing a setup.py parsetree or parts of it.  
+    :returns:    A list of strings containing a setup call each.
+    """
+
+    _setup_call_list = list()
+    for child in node.get_children(): 
+        if child.as_string()[:5] == 'setup':
+            _setup_call_list.append(child.as_string())
+        else:
+            _setup_call_list = _setup_call_list + _astroid_setup_search(child)
+
+    return _setup_call_list
 
 def _get_setup_locations(path: str) -> list:
     """ Returns a list of paths to setup.py files in a directory.
