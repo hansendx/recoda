493580f170333bd16e6d80c4b483e0a9e6ef09d8
hansendx <hansendx@hu-berlin.de>
2018-11-12 21:43:10 +0100

    [refactor] Add capability to find short Apache-2.0

--- recoda/analyse/independent/openness.py
+++ recoda/analyse/independent/openness.py
@@ -4,6 +4,7 @@ import re
 import json
 from subprocess import Popen, PIPE
 
+_APACHE_SNIPPET = 'the Apache License, Version 2.0'
 
 def license_type(project_path: str) -> str:
     """ Get the license of a project is licensed under if it is an open license. """
@@ -32,8 +33,15 @@ def _get_license(licensee_output: dict) -> str:
 
     for _license_file in licensee_output['matched_files']:
         if _license_file['matched_license'] == 'NOASSERTION':
-            if not _license:
-                _license = 'unknown'
+            if not _license or _license == 'unknown':
+                if _APACHE_SNIPPET in _license_file['content']:
+                    # Apache 2.0 has a short version,
+                    # that licensee does not seem to recognize.
+                    # So we check if we can match the license snippet,
+                    # if nothing else matches.
+                    _license = 'Apache-2.0'
+                else:
+                    _license = 'unknown'
         elif _confidence < _license_file["matcher"]["confidence"]:
             _license = _license_file["matched_license"]
             _confidence = _license_file["matcher"]["confidence"]
